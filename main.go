bda60c185133296e534fb36f7b0052a3fde7b787
Kristin Barkardottir <kristin.barkardottir@netapp.com>
2021-03-29 20:24:21 +0000

    create db, table and insert
    Signed-off-by: Kristin Barkardottir <kristin.barkardottir@netapp.com>

--- main.go
+++ main.go
@@ -8,14 +8,21 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var (
+const (
 	MariaDBRootUser     = "root"
 	MariaDBRootPassword = "secret"
 	MariaDBHost         = "127.0.0.1"
 	MariaDBClientPort   = "3306"
-	logLevel            = "info"
-	connStr             = MariaDBRootUser + ":" + MariaDBRootPassword + "@tcp(" + MariaDBHost + ":" + MariaDBClientPort + ")/"
 )
+
+type Connection struct {
+	MariaDBRootUser     string
+	MariaDBRootPassword string
+	MariaDBHost         string
+	MariaDBClientPort   string
+}
+
+var logLevel = "info"
 
 type Canines struct {
 	Id               int64
@@ -29,21 +36,30 @@ type CanineModel struct {
 	Db *sql.DB
 }
 
-func getDatabaseConnection(name string) (conn *sql.DB, err error) {
-	conn, err = sql.Open("mysql", connStr)
+func getEnv(key string, fallBack string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallBack
+}
+
+func getDatabaseConnection(dbName string, c *Connection) (conn *sql.DB, err error) {
+	cStr := c.MariaDBRootUser + ":" + c.MariaDBRootPassword + "@tcp(" + c.MariaDBHost + ":" + c.MariaDBClientPort + ")/"
+	log.Debugf("connection string %v", cStr)
+	conn, err = sql.Open("mysql", cStr)
 	if err != nil {
 		log.Fatalf("failed to connect to to mysql %v", err)
 	}
-	_, err = conn.Exec("CREATE DATABASE IF NOT EXISTS" + name)
+	_, err = conn.Exec("CREATE DATABASE IF NOT EXISTS " + dbName)
 	if err != nil {
-		log.Fatalf("could not create database %v err: %v", name, err)
+		log.Fatalf("could not create database %v err: %v", dbName, err)
 	}
 	return
 }
 
 func (canineModel CanineModel) insertCanines(canines *Canines) (int64, error) {
 	log.Debugf("Insert values %v, %v, %v, %v", canines.Breed, canines.IsHypoAllergenic, canines.LifeExpectancy, canines.Origin)
-	result, err := canineModel.Db.Exec("INSERT INTO CANINES(breed, isHypoAllergenic, lifeExpectancy, origin) values(?,?,?,?)", canines.Breed, canines.IsHypoAllergenic, canines.LifeExpectancy, canines.Origin)
+	result, err := canineModel.Db.Exec("INSERT INTO dogsDB.CANINES(breed, isHypoAllergenic, lifeExpectancy, origin) values(?,?,?,?)", canines.Breed, canines.IsHypoAllergenic, canines.LifeExpectancy, canines.Origin)
 	if err != nil {
 		log.Errorf("Unexpected result trying to insert entries %v", err)
 	} else {
@@ -55,14 +71,14 @@ func (canineModel CanineModel) insertCanines(canines *Canines) (int64, error) {
 }
 
 func (canineModel CanineModel) createTable(name string) (int64, error) {
-	log.Debugf("Create table %v: ", name)
-	result, err := canineModel.Db.Exec("CREATE TABLE IF NOT EXISTS " + name + " ( " +
-		"'Id' INT NOT NULL PRIMARY KEY AUTO_INCREMENT," +
-		"'Breed' VARCHAR(150) NOT NULL," +
-		"'IsHypoAllergenic' BOOL NOT NULL" +
-		"'LifeExpectancy' INT" +
-		"'Origin' VARCHAR(150)" +
-		")")
+	log.Debugf("CREATE TABLE IF NOT EXISTS %v: ", name)
+	result, err := canineModel.Db.Exec("CREATE TABLE IF NOT EXISTS " + name + "( " +
+		"Id INT NOT NULL PRIMARY KEY AUTO_INCREMENT," +
+		"Breed VARCHAR(150) NOT NULL," +
+		"IsHypoAllergenic BOOL NOT NULL," +
+		"LifeExpectancy INT," +
+		"Origin VARCHAR(150) ) ENGINE=INNODB;")
+
 	if err != nil {
 		log.Errorf("Encountered issues creating table %v err: %v", name, err)
 	} else {
@@ -79,29 +95,31 @@ func main() {
 		ll = log.DebugLevel
 	}
 	log.SetLevel(ll)
-	MariaDBRootUser, _ = os.LookupEnv("MARIADB_ROOT_USER")
-	MariaDBRootPassword, _ = os.LookupEnv("MARIADB_ROOT_PASSWORD")
-	MariaDBHost, _ = os.LookupEnv("MARIADB_HOST")
-	MariaDBClientPort, _ = os.LookupEnv("MARIADB_CLIENT_PORT")
+	c := Connection{
+		MariaDBRootUser:     getEnv("MARIADB_ROOT_USER", MariaDBRootUser),
+		MariaDBRootPassword: getEnv("MARIADB_ROOT_PASSWORD", MariaDBRootPassword),
+		MariaDBHost:         getEnv("MARIADB_HOST", MariaDBHost),
+		MariaDBClientPort:   getEnv("MARIADB_CLIENT_PORT", MariaDBClientPort),
+	}
+	dbName := "dogsDB"
 
-	log.Debugf("%v, %v, %v, %v", MariaDBRootPassword, MariaDBRootUser, MariaDBHost, MariaDBClientPort)
-
-	db, err := getDatabaseConnection("dogsDB")
+	db, err := getDatabaseConnection(dbName, &c)
 	if err != nil {
 		log.Fatalf("Could not connect to database %v:", err)
 	} else {
 		canineModel := CanineModel{
 			Db: db,
 		}
-		_, err1 := canineModel.createTable("Canines")
+		_, err1 := canineModel.createTable(dbName + ".CANINES")
 		if err1 != nil {
 			log.Error(err1)
 		}
+
 		canines := Canines{
-			Breed: "Schnauzer",
+			Breed:            "Schnauzer",
 			IsHypoAllergenic: true,
-			LifeExpectancy: 14,
-			Origin: "Germany",
+			LifeExpectancy:   14,
+			Origin:           "Germany",
 		}
 		rowsAffected, err2 := canineModel.insertCanines(&canines)
 		if err2 != nil {
@@ -117,6 +135,5 @@ func main() {
 
 		}
 	}
-
 
 }
